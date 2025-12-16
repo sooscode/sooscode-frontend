503e3ee1bc7f8d3dabb3ded1276cd41484998de1
jhy <idpc8090@gmail.com>
2025-12-17 08:50:33 +0900

    병합

--- src/features/classroom/components/snapshot/SnapshotPanel.jsx
+++ src/features/classroom/components/snapshot/SnapshotPanel.jsx
@@ -146,9 +146,6 @@ const SnapshotPanel = () => {
     return (
         <div className={styles.container}>
 
-            <div className={styles.header}>
-                {/*<SnapshotSaveFeature />*/}
-
             <SnapshotSearchFilter
                 onSearch={handleSearch}
                 value={filters.title}
